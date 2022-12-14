262dc504b3bfa60916140545c8e2380c2238d6ac
Hecen1980 <hector_enrique_1980@hotmail.com>
2022-12-13 19:33:32 -0500

    implementación botón buscar en home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +11,11 @@ import { LoginComponent } from './paginas-comerce/login/login.component';
 import { ComponentescomerceModule } from './componentescomerce/componentescomerce.module';
 import { PaginasComerceModule } from './paginas-comerce/paginas-comerce.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {HttpClientModule} from '@angular/common/http';
+import { MatAutocompleteModule} from "@angular/material/autocomplete";
+
+
+
 
 
 @NgModule({
@@ -21,14 +26,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+     AppRoutingModule,
     ComponentescomerceModule,
     PaginasComerceModule,
     FormsModule,
-    BrowserAnimationsModule
- 
-
-
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
